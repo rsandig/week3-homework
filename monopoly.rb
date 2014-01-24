baba23262ebb9f70587f3a0ff87d040c5e11c278
Robert L. Sandig <rsandig@bigpond.net.au>
2014-01-23 19:26:34 -0600

    Homework 3 completed

--- monopoly.rb
+++ monopoly.rb
@@ -13,26 +13,35 @@ special_squares = { :good => ["Go", "Free Parking", "Just Visiting"],
 
 #Q1. How much does it cost to buy Baltic?
 #A1: Here's the answer:
-puts cheap_property[:price]
+#puts cheap_property[:price]
+balticPrice = cheap_property[:price]
+puts "It costs #{balticPrice} to buy Baltic"
+#60
 
 #Q2. How much does it cost to buy Boardwalk?
 #A2: Your code goes here:
-
-
+# puts rich_property[:price]
+boardwalkPrice = rich_property[:price]
+puts "Boardwalk costs #{boardwalkPrice}"
+#400
 
 #Q3. How much is rent for Baltic?
 #A3: Your code goes here:
-
-
+# puts cheap_property[:rent]
+balticRent = cheap_property[:rent]
+puts "Baltic rents for #{balticRent}"
+#2
 
 #Q4. How much does it cost to buy New York Ave?
 #A4: Your code goes here:
-
-
+ny_ave = more_properties[2]
+# puts ny_ave[:price]
+ny_avePrice = ny_ave[:price]
+puts "New York Ave is going for #{ny_avePrice}"
+#180
 
 #Q5: How many "good" special squares are there?
 #A5: Your code goes here:
-
-
-
-
+# puts special_squares[:good].count
+puts "There are #{special_squares[:good].count} \"good\'' special squares"
+#3
